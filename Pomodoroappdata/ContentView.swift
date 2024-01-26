22137c8be233f03fc891ed031e2af6d1fc419513
AliSiddique <alisiddique10@hotmail.com>
2024-01-26 22:02:46 +0000

    D

--- Pomodoroappdata/ContentView.swift
+++ Pomodoroappdata/ContentView.swift
@@ -6,75 +6,17 @@
 //
 
 import SwiftUI
+
 struct ContentView: View {
-    @ObservedObject var viewModel = ViewModel()
-    @State private var selectedTab = 0
-    @State private var indicatorOffset: CGFloat = -3.5
-    
-    init() {
-        _selectedTab = State(initialValue: 0)
-        _indicatorOffset = State(initialValue: UIScreen.main.bounds.width / -3.5)
-    }
-    
+    @EnvironmentObject var pomodoroModel: PomodoroModel
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color.black.edgesIgnoringSafeArea(.all)
-                
-                VStack {
-                    HStack {
-                        Spacer()
-                        Text("Pomodoro")
-                            .foregroundColor(selectedTab == 0 ? .white : .gray)
-                            .onTapGesture {
-                                selectedTab = 0
-                                indicatorOffset = UIScreen.main.bounds.width / -3.5
-                            }
-                        Spacer()
-                        Text("Short break")
-                            .foregroundColor(selectedTab == 1 ? .white : .gray)
-                            .onTapGesture {
-                                selectedTab = 1
-                                indicatorOffset = 0
-                            }
-                        Spacer()
-                        Text("Long break")
-                            .foregroundColor(selectedTab == 2 ? .white : .gray)
-                            .onTapGesture {
-                                selectedTab = 2
-                                indicatorOffset = UIScreen.main.bounds.width / 3.5
-                            }
-                        Spacer()
-                    }
-                    .font(.title3)
-                    .bold()
-                    .padding()
-                    
-                    Rectangle()
-                        .fill(Color(.blue))
-                        .frame(width: UIScreen.main.bounds.width / 8, height: 2)
-                        .offset(x: indicatorOffset, y: 0)
-                    
-                    TabView(selection: $selectedTab) {
-                        WorkTimerView(viewModel: viewModel)
-                            .tag(0)
-                        
-                        ShortTimerView(viewModel: viewModel)
-                            .tag(1)
-                        
-                        LongTimerView(viewModel: viewModel)
-                            .tag(2)
-                    }
-                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                }
-            }
-        }
-        .navigationBarHidden(true)
-        .navigationViewStyle(StackNavigationViewStyle()) // Use StackNavigationViewStyle for iPad
+        HomeView()
+            .environmentObject(pomodoroModel)
     }
 }
 
 
 #Preview {
     ContentView()
+        .environmentObject(PomodoroModel())
 }
